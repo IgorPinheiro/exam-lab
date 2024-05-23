b5bf282ba75761c7edcd4ff10a4f92848e2475e4
Igor <igor.warlog@gmail.com>
2024-05-22 23:26:44 -0300

    Add message, criação de regras e contrução da pagina de login

--- users/views.py
+++ users/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 # Create your views here.
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha =  request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senha não está igual a confirmação, por favor tente novamente!' )
+            return redirect('/users/cadastro')
+        if len(senha) < 6:
+            return redirect('/users/cadastro')
+        
+        if len(primeiro_nome) == 0 or len(email) == 0:
+            messages.add_message(request, constants.ERROR, 'Nome ou email não podem ser vazios')
+            return redirect('/users/cadastro')
+        
+        
+        if User.objects.filter(username = username).exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe!' )
+            return redirect('cadastro')
+        try:
+            user = User.objects.create_user(
+                first_name = primeiro_nome,
+                last_name = ultimo_nome,
+                username = username,
+                email = email,
+                password=senha
+            )
+            messages.add_message(request, constants.SUCCESS, 'USUÁRIO CADASTRADO COM SUCESSO!')
+        except:
+            messages.add_message(request, constants.ERROR, 'Error no sistema, tente novamente')
+            return redirect('/users/cadastro')
+        
+        return redirect('/users/cadastro')
+        return HttpResponse('passou')
+    
+
+
+
 
 def login(request):
     return render(request, 'login.html')
